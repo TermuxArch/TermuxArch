94fb4158cc37bb8f2d77f2d3a13af83ccdc4385f
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-09-28 22:20:23 -0400

    modified:   setupTermuxArch.bash 	modified:   setupTermuxArch.sha512 	modified:   setupTermuxArch.tar.gz

--- setupTermuxArch.bash
+++ setupTermuxArch.bash
@@ -9,7 +9,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-VERSIONID="v1.6.7.id5678"
+VERSIONID="v2.0.id0400"
 ## INIT FUNCTIONS ##############################################################
 _ARG2DIR_() {  # Argument as ROOTDIR.
 	ARG2="${@:2:1}"
