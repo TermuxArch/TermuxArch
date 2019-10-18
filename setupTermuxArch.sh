24c01eb055da30a4f46cb76178ef18bd98e97717
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-17 22:51:27 -0400

    modified:   getimagefunctions.bash 	modified:   printoutstatements.bash 	modified:   setupTermuxArch.bash 	modified:   setupTermuxArch.sh 	modified:   setupTermuxArch.sha512 	modified:   setupTermuxArch.tar.gz 	modified:   ztree.sha512.sum

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -7,7 +7,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-VERSIONID="2.0.1.9"
+VERSIONID="2.0.2"
 ## INIT FUNCTIONS ##############################################################
 _ARG2DIR_() {  # Argument as ROOTDIR.
 	ARG2="${@:2:1}"
@@ -809,5 +809,4 @@ elif [[ "${1//-}" = [Ww]* ]] ; then
 else
 	_PRINTUSAGE_
 fi
-
-# setupTermuxArch.bash EOF
+# setupTermuxArch.sh EOF
