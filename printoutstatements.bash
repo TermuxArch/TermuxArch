b21983925aa64e70cbde80a7e438ec4431248424
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-22 00:30:31 -0400

    modified:   printoutstatements.bash 	modified:   setupTermuxArch.bash 	modified:   setupTermuxArch.sh 	modified:   setupTermuxArch.sha512 	modified:   setupTermuxArch.tar.gz 	modified:   sha512.sum

--- printoutstatements.bash
+++ printoutstatements.bash
@@ -15,7 +15,7 @@ FLHDR1[1]="# IFS=$'\\n\\t'"
 FLHDR1[2]="set -Eeuo pipefail"
 FLHDR1[3]="# shopt -s nullglob globstar"
 FLHDR1[4]="unset LD_PRELOAD"
-FLHDR1[5]="VERSIONID=2.0.7"
+FLHDR1[5]="VERSIONID=2.0.8"
 FLHDR1[6]=" "
 FLHDRP[0]="## BEGIN #######################################################################"
 FLHDRP[1]=""
@@ -163,7 +163,7 @@ _PRINTMAX_() {
 }
 
 _PRINTMD5CHECK_() {
-	printf "\n\033[0;34m 🕛 > 🕠 \033[1;34mChecking download integrity with Termux busybox md5sum.  \033[37;1mThis may take a little while:\n\n\033[1;33m"
+	printf "\n\033[0;34m 🕛 > 🕠 \033[1;34mChecking download integrity with md5sum.  \033[37;1mThis may take a little while:\n\n\033[1;33m"
 }
 
 _PRINTMD5ERROR_() {
@@ -190,7 +190,7 @@ _PRINTFOOTER_() {
 }
 
 _PRINTFOOTER2_() {
-	printf "\033[1;34mArch Linux in Termux PRoot is installed in $INSTALLDIR.  This project is in active development.  Contributions to this project are welcome; See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS for information.  The documentation repository for TermuxArch https://sdrausty.github.io/TermuxArch/docs/ is a Termux Arch submodule that is located at https://github.com/sdrausty/docsTermuxArch.  Pull requests and contributions through the issues pages are open to improve the ux (user experience) and this Termux PRoot installation script.\n\nUse \033[1;32m~$PRINTROOTDIR/$STARTBIN \033[1;34mand \033[1;32m$STARTBIN \033[1;34min a BASH shell to launch Arch Linux in Termux PRoot for future sessions.  If you are new to *nix, http://tldp.org has *nix documentation.  See https://wiki.archlinux.org/index.php/IRC_channel for available Arch Linux IRC channels.\n\n\033[0m"
+	printf "\033[1;34mArch Linux in Termux PRoot is installed in $INSTALLDIR.  This project is in active development.  Contributions to this project are welcome; See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS for information.  The documentation repository for TermuxArch https://sdrausty.github.io/TermuxArch/docs/ is a Termux Arch submodule that is located at https://github.com/sdrausty/docsTermuxArch.  Pull requests and contributions through the issues pages are open to improve the ux (user experience) and this Termux PRoot installation script.\n\nUse \033[1;32m~$PRINTROOTDIR/$STARTBIN \033[1;34mand \033[1;32m$STARTBIN \033[1;34min a BASH shell to launch Arch Linux in Termux PRoot for future sessions.  If you are new to *nix, http://tldp.org has *nix documentation.  See https://wiki.archlinux.org/index.php/IRC_channel for available Arch Linux IRC channels.  \\e[0;34mIf you get a '/usr/bin/env' not found error, please comment at https://github.com/termux/proot/issues/76\n\n\033[0m"
 }
 
 _PRINTROOTDIRFUNCTION_() {
