1de11bbbdf92db4b3d0fc3efd59eda3dfc779248
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-04 11:29:36 -0500

    change 105133024 on 20191204

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -7,7 +7,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-VERSIONID=2.0.23
+VERSIONID=2.0.24
 ## INIT FUNCTIONS ##############################################################
 _ARG2DIR_() {  # argument as ROOTDIR
 	ARG2="${@:2:1}"
@@ -58,7 +58,7 @@ _CHK_() {
 _CHKDWN_() {
 	if sha512sum -c setupTermuxArch.sha512 1>/dev/null 
 	then
-		printf "\\e[0;34m%s\\e[1;34m%s%s\\e[1;32m%s\\n\\n" " 🕛 > 🕐 " "TermuxArch download: " "OK"
+		printf "\\e[0;34m%s\\e[1;34m%s\\e[1;32m%s\\n\\n" " 🕛 > 🕐 " "TermuxArch download: " "OK"
 		bsdtar -xpf setupTermuxArch.tar.gz 
 	else
 		_PRINTSHA512SYSCHKER_
