803330ca6121a8e5b87579c8c9714c5b8605c346
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-20 02:44:41 -0500

    commit 191222883 on 20191220

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -7,7 +7,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-VERSIONID=2.0.44
+VERSIONID=2.0.45
 ## INIT FUNCTIONS ##############################################################
 _ARG2DIR_() {  # argument as ROOTDIR
 	ARG2="${@:2:1}"
