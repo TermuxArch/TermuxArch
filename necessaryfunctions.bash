7a714c004f24cd29df56cb6eabfda8b7cc70261e
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-08-06 10:35:05 -0400

    commit 155434500 on 20200806

--- necessaryfunctions.bash
+++ necessaryfunctions.bash
@@ -220,9 +220,9 @@ _MAKEFINISHSETUP_() {
 	 	fi
 		if [[ "$CPUABI" = "$CPUABIX86" ]] || [[ "$CPUABI" = "$CPUABIX86_64" ]]
 		then
-			printf "%s\\n" "pacman -S gzip sed sudo" >> root/bin/"$BINFNSTP"
+			printf "%s\\n" "pacman -S gzip sed sudo --noconfirm --color=always 2>/dev/null ||:\\n" >> root/bin/"$BINFNSTP"
 		else
-			printf "%s\\n" "pacman -S sudo" >> root/bin/"$BINFNSTP"
+			printf "%s\\n" "pacman -S sudo --noconfirm --color=always 2>/dev/null ||:\\n" >> root/bin/"$BINFNSTP"
 		fi
 		_DOKEYS_
 	fi
