a7f8fb1ac591a1836e93ebe5f7b4fc40e7617cbf
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-08-06 05:46:32 -0400

    commit 752811658 on 20200806

--- archlinuxconfig.bash
+++ archlinuxconfig.bash
@@ -13,7 +13,7 @@ _ADDAUSER_() {
 		exit 201
 	else
 		useradd -s /bin/bash "\$1" -U
-		usermod "$1" -g wheel
+		usermod "\$1" -g wheel
 		[[ -f /etc/sudoers ]] && printf "%s\\n" "\$1 ALL=(ALL) ALL" >> /etc/sudoers
 		sed -i "s/\$1:x/\$1:/g" /etc/passwd
 		cp -r /root /home/"\$1"
