85581ca4f3486a52d9239692b2d195aac1b88bd9
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-08-19 05:59:41 -0400

    commit 900697852 on 20200819

--- updateTermuxArch.bash
+++ updateTermuxArch.bash
@@ -36,7 +36,7 @@ trap _SGSATRPSIGNAL_ HUP INT TERM
 trap _SGSATRPQUIT_ QUIT
 
 _GSA_() { # add or update module 
-	[[ -f .cong/RONGSA ]] && ((git submodule update --init --recursive --remote "$1") || (printf "\\n\\n%s\\n" "Cannot update module $2 : Continuing...")) || ((git submodule add $3 https:/$SIAD$OUNA/$2 $1 && touch .conf/RONGSA) || (printf "\\n\\n%s\\n" "Cannot add module $2 : Continuing..."))
+	[[ -f .cong/RONGSA ]] && ((git submodule update --init --recursive --remote "$1") || (printf "\\n\\n%s\\n" "Cannot update module $2 : Continuing...")) || ((git submodule add --depth 1 $3 https:/$SIAD$OUNA/$2 $1 && touch .conf/RONGSA) || (printf "\\n\\n%s\\n" "Cannot add module $2 : Continuing..."))
 	 sleep 0."$(shuf -i 24-72 -n 1)"	# latency support
 }
 git pull || printf "\\n\\n%s\\n" "Cannot git pull : Continuing..."
