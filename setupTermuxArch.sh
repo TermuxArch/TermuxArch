edf2c126ca3cf7f7e7c40636625d2c22931ed4c1
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-08-24 07:25:50 -0400

    commit 415621102 on 20200824

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-VERSIONID=2.0.369
+VERSIONID=2.0.370
 ## INIT FUNCTIONS ##############################################################
 _STRPERROR_() { # run on script error
 	local RV="$?"
