841ad181bb05cce100f5fa62f6632523b3b6d44b
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-09-13 22:13:57 -0400

    commit 014357826 on 20200913

--- setupTermuxArch.bash
+++ setupTermuxArch.bash
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 umask 022
-VERSIONID=2.0.551
+VERSIONID=2.0.552
 ## INIT FUNCTIONS ##############################################################
 _STRPERROR_() { # run on script error
 	local RV="$?"
