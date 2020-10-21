0d5c115400eae1f26bf5c7877613687abd59c5e5
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-10-21 00:00:12 -0400

    commit 464062421 on 20201021

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -7,7 +7,7 @@
 IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
-VERSIONID=2.0.618
+VERSIONID=2.0.619
 umask 0022
 unset LD_PRELOAD
 ## INIT FUNCTIONS ##############################################################
