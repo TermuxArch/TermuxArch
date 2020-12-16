85760134497c5d8546b69456e4acda42de22d4e0
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-12-16 15:14:01 -0500

    commit 786364063 on 20201216

--- qemugeti686.bash
+++ qemugeti686.bash
@@ -38,13 +38,19 @@ fi
 LATESTI686="$(grep i686 sha512sums |tail -n 1)"
 printf "%s\\n\\n" "Found filename '${LATESTI686##* }' with sha512sum ${LATESTI686%% *}."
 [ -f "${LATESTI686##* }" ] && printf "%s\\n\\n" "Found filename '${LATESTI686##* }'." || curl -OL "https://mirror.math.princeton.edu/pub/archlinux32/archisos/${LATESTI686##* }"
+if [ ! -f "checked${LATESTI686##* }" ]
+then
 printf "%s\\n\\n" "Checking file '${LATESTI686##* }' with sha512sum."
 LATESTI686ISO="$(sha512sum "${LATESTI686##* }")"
 if [[ "$LATESTI686ISO" == "$LATESTI686" ]]
 then
+touch "checked${LATESTI686##* }"
 _BOOTISO_ "${LATESTI686##* }"
 else
 printf "%s\\n\\n" "Checking file '${LATESTI686##* }' with sha512sum failed;  Removing files '${LATESTI686##* }' and sha512sums."
 rm -f "${LATESTI686##* }" sha512sums
+fi
+else
+_BOOTISO_ "${LATESTI686##* }"
 fi
 ## qemugeti686.bash EOF
