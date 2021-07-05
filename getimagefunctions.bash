3c6d8f8c81a0b62dddce3321efdb4890618f1090
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-07-05 17:33:29 -0400

    commit 927535160 on 20210705

--- getimagefunctions.bash
+++ getimagefunctions.bash
@@ -9,19 +9,19 @@ _FTCHIT_() {
 _PRINT_DOWNLOADING_FTCHIT_
 if [[ "$DM" = aria2 ]]
 then
-aria2c -c -Z https//"$CMIRROR/$RPATH/$IFILE".md5 https//"$CMIRROR/$RPATH/$IFILE"
+aria2c -c -Z http://"$CMIRROR/$RPATH/$IFILE".md5 http://"$CMIRROR/$RPATH/$IFILE"
 elif [[ "$DM" = axel ]]
 then
-axel https//"$CMIRROR/$RPATH/$IFILE".md5
-axel https//"$CMIRROR/$RPATH/$IFILE"
+axel http://"$CMIRROR/$RPATH/$IFILE".md5
+axel http://"$CMIRROR/$RPATH/$IFILE"
 elif [[ "$DM" = lftp ]]
 then
-lftpget -c https//"$CMIRROR/$RPATH/$IFILE".md5 https//"$CMIRROR/$RPATH/$IFILE"
+lftpget -c http://"$CMIRROR/$RPATH/$IFILE".md5 http://"$CMIRROR/$RPATH/$IFILE"
 elif [[ "$DM" = wget ]]
 then
-wget "$DMVERBOSE" -c --show-progress -N https//"$CMIRROR/$RPATH/$IFILE".md5 https//"$CMIRROR/$RPATH/$IFILE"
+wget "$DMVERBOSE" -c --show-progress -N http://"$CMIRROR/$RPATH/$IFILE".md5 http://"$CMIRROR/$RPATH/$IFILE"
 else
-curl "$DMVERBOSE" -C - --fail --retry 4 -OL {"https//$CMIRROR/$RPATH/$IFILE.md5,https//$CMIRROR/$RPATH/$IFILE"}
+curl "$DMVERBOSE" -C - --fail --retry 4 -OL {"http://$CMIRROR/$RPATH/$IFILE.md5,http://$CMIRROR/$RPATH/$IFILE"}
 fi
 }
 
@@ -30,23 +30,23 @@ FSTND=1
 _PRINTCONTACTING_
 if [[ "$DM" = aria2 ]]
 then
-aria2c https//"$CMIRROR" 1>"$TAMPDIR/global2localmirror"
+aria2c http://"$CMIRROR" 1>"$TAMPDIR/global2localmirror"
 NLCMIRROR="$(grep Redirecting "$TAMPDIR/global2localmirror" | awk {'print $8'})"
 _PRINTDONE_
 _PRINTDOWNLOADINGFTCH_
 aria2c -c -m 4 -Z "$NLCMIRROR/$RPATH/$IFILE".md5 "$NLCMIRROR/$RPATH/$IFILE"
 elif [[ "$DM" = axel ]]
 then
-axel -vv https//"$CMIRROR" 1 > "$TAMPDIR/global2localmirror"
+axel -vv http://"$CMIRROR" 1 > "$TAMPDIR/global2localmirror"
 NLCMIRR="$(grep downloading "$TAMPDIR/global2localmirror" | awk {'print $5'})"
 NLCMIRROR="${NLCMIRR::-3}"
 _PRINTDONE_
 _PRINTDOWNLOADINGFTCH_
-axel -a https//"$NLCMIRROR/$RPATH/$IFILE".md5
-axel -a https//"$NLCMIRROR/$RPATH/$IFILE"
+axel -a http://"$NLCMIRROR/$RPATH/$IFILE".md5
+axel -a http://"$NLCMIRROR/$RPATH/$IFILE"
 elif [[ "$DM" = lftp ]]
 then
-lftp -e get https//"$CMIRROR" 2>&1 | tee>"$TAMPDIR/global2localmirror"
+lftp -e get http://"$CMIRROR" 2>&1 | tee>"$TAMPDIR/global2localmirror"
 NLCMI="$(grep direct "$TAMPDIR/global2localmirror" | awk {'print $5'})"
 NLCMIRR="${NLCMI//\`}"
 NLCMIRROR="${NLCMIRR//\'}"
@@ -55,13 +55,13 @@ _PRINTDOWNLOADINGFTCH_
 lftpget -c "$NLCMIRROR/$RPATH/$IFILE".md5 "$NLCMIRROR/$RPATH/$IFILE"
 elif [[ "$DM" = wget ]]
 then
-wget -v -O/dev/null https//"$CMIRROR" 2>"$TAMPDIR/global2localmirror"
+wget -v -O/dev/null "$CMIRROR" 2>"$TAMPDIR/global2localmirror"
 NLCMIRROR="$(grep Location "$TAMPDIR/global2localmirror" | awk {'print $2'})"
 _PRINTDONE_
 _PRINTDOWNLOADINGFTCH_
 wget "$DMVERBOSE" -c --show-progress "$NLCMIRROR/$RPATH/$IFILE".md5 "$NLCMIRROR/$RPATH/$IFILE"
 else
-curl -v https//"$CMIRROR" &> "$TAMPDIR/global2localmirror"
+curl -v "$CMIRROR" &> "$TAMPDIR/global2localmirror"
 NLCMIRROR="$(grep Location "$TAMPDIR/global2localmirror" | awk {'print $3'})"
 NLCMIRROR="${NLCMIRROR%$'\r'}" # remove trailing carrage return: strip bash variable of non printing characters
 _PRINTDONE_
@@ -74,28 +74,28 @@ _GETIMAGE_() {
 _PRINTDOWNLOADINGX86_
 if [[ "$DM" = aria2 ]]
 then
-aria2c https//"$CMIRROR/$RPATH/$IFILE".md5
+aria2c http://"$CMIRROR/$RPATH/$IFILE".md5
 _ISX86_
-aria2c -c https//"$CMIRROR/$RPATH/$IFILE"
+aria2c -c http://"$CMIRROR/$RPATH/$IFILE"
 elif [[ "$DM" = axel ]]
 then
-axel https//"$CMIRROR/$RPATH/$IFILE".md5
+axel http://"$CMIRROR/$RPATH/$IFILE".md5
 _ISX86_
-axel https//"$CMIRROR/$RPATH/$IFILE"
+axel http://"$CMIRROR/$RPATH/$IFILE"
 elif [[ "$DM" = lftp ]]
 then
-lftpget https//"$CMIRROR/$RPATH"/md5sums.txt
+lftpget http://"$CMIRROR/$RPATH"/md5sums.txt
 _ISX86_
-lftpget -c https//"$CMIRROR/$RPATH/$IFILE"
+lftpget -c http://"$CMIRROR/$RPATH/$IFILE"
 elif [[ "$DM" = wget ]]
 then
-wget "$DMVERBOSE" -N --show-progress https//"$CMIRROR/$RPATH/"md5sums.txt
+wget "$DMVERBOSE" -N --show-progress http://"$CMIRROR/$RPATH/"md5sums.txt
 _ISX86_
-wget "$DMVERBOSE" -c --show-progress https//"$CMIRROR/$RPATH/$IFILE"
+wget "$DMVERBOSE" -c --show-progress http://"$CMIRROR/$RPATH/$IFILE"
 else
-curl "$DMVERBOSE" --fail --retry 4 -OL https//"$CMIRROR/$RPATH"/md5sums.txt
+curl "$DMVERBOSE" --fail --retry 4 -OL http://"$CMIRROR/$RPATH"/md5sums.txt
 _ISX86_
-curl "$DMVERBOSE" -C - --fail --retry 4 -OL https//"$CMIRROR/$RPATH/$IFILE"
+curl "$DMVERBOSE" -C - --fail --retry 4 -OL http://"$CMIRROR/$RPATH/$IFILE"
 fi
 }
 
