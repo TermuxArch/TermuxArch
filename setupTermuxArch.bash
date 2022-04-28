720c1337640b17c0e1e695a41f41330d988f947b
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-04-27 20:08:27 -0400

    commit 20220427.18d8e5b7

--- setupTermuxArch.bash
+++ setupTermuxArch.bash
@@ -6,7 +6,7 @@
 set -Eeuo pipefail
 shopt -s  extglob nullglob globstar
 unset LD_PRELOAD
-VERSIONID=2.1.545
+VERSIONID=2.1.546
 _STRPEROR_() { # run on script error
 local RV="$?"
 printf "\\e[1;48;5;138m %s" "ＴｅｒｍｕｘＡｒｃｈ NOTICE:  Generated script signal received ${RV:-UNKNOWN} near or at line number ${1:-UNKNOWN} by '${2:-UNKNOWNCOMMAND}'!  "
@@ -23,7 +23,6 @@ if [[ "$RV" != 0 ]]
 then
 _PTSTRPXT_
 fi
-exit
 if [[ "$RV" = 0 ]]
 then
 printf "\\e[0;32mCommand \\e[1;32m'%s' \\e[0;32mversion %s\\e[1;34m: \\e[1;32m%s\\n" "${STRNRG:-}" "${VERSIONID:-}" "DONE 🏁 "
