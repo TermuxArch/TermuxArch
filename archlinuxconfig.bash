465dff7639a3c99ab5b9ca165d63589fd9333c3a
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-06-05 18:43:48 -0400

    commit 20220605.b75d0d0c

--- archlinuxconfig.bash
+++ archlinuxconfig.bash
@@ -719,7 +719,7 @@ PSCMMT="(please quote multiple words)"
 XLCD00="'\$SRPTNM f 'machine virtual'' \$PSCMMT"
 XLCD0L="'\$SRPTNM find 'machine virtual'' \$PSCMMT"
 XLCD01="'\$SRPTNM b libguestfs'"
-XLCD02="'\$SRPTNM v libguestfs'"
+XLCD02="'\$SRPTNM s libguestfs'"
 XLCD03="'\$SRPTNM g 'guestfish --help'' \$PSCMMT"
 XLCD04="'\$SRPTNM l 'guestfish --help'' \$PSCMMT"
 # builtin help string variables end
@@ -751,7 +751,7 @@ v[irt-inspector 'cmd cmd']  run either virt-inspector (default) or run command '
 [ -n "\${1:-}" ] && [[ "\${1//-}" = [Vv]* ]] && { [ -d "\$HOME"/libguestfs ] && cd "\$HOME"/libguestfs && printf '%s\n' "Running command '\$HOME/libguestfs/run \$HOME/libguestfs/fish/virt-inspector \${2:-}' in directory '\$PWD'..." && \$HOME/libguestfs/run "\$HOME/libguestfs/fish/guestfish \${2:-}" && exit || { printf '\\e[0;32m%s' "\$HLPSTG" ; exit ; } ; }
 [ -n "\${1:-}" ] && { for ARG1 in '/' '?' {0..9} Aa Cc Dd Ee Hh Ii Jj Kk Oo Qq Rr Tt Uu Ww Xx Yy Zz ; do [[ "\${1//-}" = ["\$ARG1"]* ]] && { printf '\\e[0;32m%s' "\$HLPSTG" ; exit ; } ; done ; }
 # makelibguestfs begin
-[ -z "\${1:-}" ] && { { [ -x "\$HOME"/libguestfs/fish/guestfish ] && TMRCMDVL="./run guestfish --help" && cd "\$HOME"/libguestfs && printf '%s\n' "Running command '\$TMRCMDVL' in directory '\$PWD'..." && \$TMRCMDVL && exit ; } || printf "\\e[48;5;22m%s\\n" "Command \$SRPTNM is attempting to build and install 'libguestfs' for computer architecture '\$NMCMND'..." ; }
+[ -z "\${1:-}" ] && { { [ -f "\$HOME"/libguestfs/fish/guestfish.1 ] && TMRCMDVL="man \$HOME/libguestfs/fish/guestfish.1" && printf '%s\n' "Running command '\$TMRCMDVL' in directory '\$PWD'..." && \$TMRCMDVL && exit ; } || printf "\\e[48;5;22m%s\\n" "Command \$SRPTNM is attempting to build and install 'libguestfs' for computer architecture '\$NMCMND'..." ; }
 # libguestfs dependencies
 GTFSDPND=(
 acl
